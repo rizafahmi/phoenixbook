bd1d7ee9b4c94be45b5ea1dbd09eb6bf83851ae8
Riza Fahmi <rizafahmi@gmail.com>
2015-05-17 17:22:56 +0700

    Add hello route

--- codes/phoenix_jobs/web/router.ex
+++ codes/phoenix_jobs/web/router.ex
@@ -16,6 +16,7 @@ defmodule PhoenixJobs.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/hello", HelloController, :index
   end
 
   # Other scopes may use custom stacks.
